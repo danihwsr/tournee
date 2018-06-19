df6d3506ae9059d07ea1ce5c2e5f6cc8ed41da11
Daniel <hoppdani@web.de>
2018-06-20 01:59:22 +0200

    new package

--- src/main/java/danihwsr/tournee/init/DatabaseSeeder.java
+++ src/main/java/danihwsr/tournee/init/DatabaseSeeder.java
@@ -1,5 +1,7 @@
-package danihwsr.tournee;
+package danihwsr.tournee.init;
 
+import danihwsr.tournee.web.UserRepository;
+import danihwsr.tournee.web.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +22,16 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         User u1 = new User(
-                "daniel", "knauser", "dooniel", "dooniel@web.de"
+                "daniel", "knauser", "dooniel", "dooniel@web.de", 28
         );
         User u2 = new User(
-                "max", "mustermann", "xXx_max_xXx", "max@gmail.com"
+                "max", "mustermann", "xXx_max_xXx", "max@gmail.com", 32
         );
         User u3 = new User(
-                "max", "musterfrau", "69_max_69", "maxi@hotmail.de"
+                "max", "musterfrau", "69_max_69", "maxi@hotmail.de", 18
         );
         User u4 = new User(
-                "peter", "meier", "the_meiernator", "meier@peter.org"
+                "peter", "meier", "the_meiernator", "meier@peter.org", 56
         );
 
         // delete all documents in collection 'users'
