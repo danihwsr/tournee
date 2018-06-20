b981bc1ec58e6d5a52026de3cea51ff97c351511
Daniel <hoppdani@web.de>
2018-06-20 14:11:22 +0200

    frist try testing with mockito

--- src/test/java/danihwsr/tournee/TourneeApplicationTests.java
+++ src/test/java/danihwsr/tournee/TourneeApplicationTests.java
@@ -1,16 +1,53 @@
 package danihwsr.tournee;
+import danihwsr.tournee.web.User;
+import danihwsr.tournee.web.UserController;
 
+import danihwsr.tournee.web.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(UserController.class)
 public class TourneeApplicationTests {
 
+    @Autowired
+    private MockMvc mock;
+
+    @MockBean
+    private UserService service;
+
     @Test
-    public void contextLoads() {
+    public void getAllUsersShouldReturnUsersFromService() throws Exception {
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+
+        when(service.getAllUsers()).thenReturn(users);
+
+        this.mock.perform(get("/users/all"))
+                .andDo(print())
+                .andExpect(status().isOk())
+        .andExpect(jsonPath("$", hasSize(4)));
+
     }
 
 }
