fedfe4976476897d603cb76e68a026597f4b050d
Daniel <hoppdani@web.de>
2018-06-29 18:51:04 +0200

    [ISSUE #7]: changed requestMapping to ${httpMethod}Mapping

--- src/main/java/danihwsr/tournee/web/UserController.java
+++ src/main/java/danihwsr/tournee/web/UserController.java
@@ -1,30 +1,23 @@
 package danihwsr.tournee.web;
 
-import danihwsr.tournee.UserAlreadyExistsException;
 import danihwsr.tournee.UserNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List; // interface
-import javax.validation.Valid;
-import javax.validation.Validation;
-import javax.validation.Validator;
+import java.util.List;
 
-// due to springframework.boot.autoconfigure, this controller will be automatically registered
 @RestController
 // main route of this controller
 @RequestMapping(value = "/users")
 public class UserController {
 
-    private List<User> users;
     private UserService userService;
-    private Validator v = Validation.buildDefaultValidatorFactory().getValidator();
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     // subroute: get /users/all
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    @GetMapping(value = "/all")
     public List<User> getAll() {
         // when declaring class attributes as private
         //+ make sure to implement public getters for the attribute
@@ -32,24 +25,22 @@ public class UserController {
         return this.userService.getAllUsers();
     }
 
-    // get /users/{nickName}
-    @RequestMapping(value = "/{nickName}", method = RequestMethod.GET)
+    @GetMapping(value = "/{nickName}")
     public User getUserByNick(@PathVariable String nickName) throws UserNotFoundException {
         return this.userService.getUserByNickname(nickName);
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    // try to parse the json object in the request body to a User instance
+    @PostMapping(value = "/create")
     public User createUser(@RequestBody User user) throws Exception {
         return this.userService.createUser(user);
     }
 
-    @RequestMapping(value = "/delete/{userId}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/delete/{userId}")
     public List<User> deleteUser(@PathVariable String userId) {
         return this.userService.deleteUser(userId);
     }
 
-    @RequestMapping(value = "/update/{userId}", method = RequestMethod.PUT)
+    @PutMapping(value = "/update/{userId}")
     public User updateUser(@PathVariable String userId, @RequestBody User user) throws Exception {
         return this.userService.updateUser(userId, user);
     }
