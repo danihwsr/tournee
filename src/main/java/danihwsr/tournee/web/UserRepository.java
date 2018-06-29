56519d6e2cff840e27b96ae1bd1082d5577e83fb
Daniel <hoppdani@web.de>
2018-06-29 19:54:23 +0200

    [ISSUE #3]: removed unused import

--- src/main/java/danihwsr/tournee/web/UserRepository.java
+++ src/main/java/danihwsr/tournee/web/UserRepository.java
@@ -2,7 +2,6 @@ package danihwsr.tournee.web;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
